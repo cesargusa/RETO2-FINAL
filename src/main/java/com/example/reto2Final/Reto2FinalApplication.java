57cc21b508e771a2bb38f0305d7f808061c12f4e
cesar gutiérrez <cesargusa-97@hotmail.com>
2021-03-02 15:04:51 +0100

    First Commit

---
+++ src/main/java/com/example/reto2Final/Reto2FinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.reto2Final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reto2FinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto2FinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reto2Final/Reto2FinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reto2Final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reto2FinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
